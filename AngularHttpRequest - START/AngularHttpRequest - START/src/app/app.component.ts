42deb4064c3bb45740cc0a0a1afec536065e6ee0
Sanket Jaiswal <sanket.jaiswal@heaps.ai>
2024-02-02 13:19:37 +0530

    implemented get , post, delete functionality

--- AngularHttpRequest - START/AngularHttpRequest - START/src/app/app.component.ts
+++ AngularHttpRequest - START/AngularHttpRequest - START/src/app/app.component.ts
@@ -2,6 +2,8 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { map } from 'rxjs';
 import { Product } from './model/products';
+import { productService } from './service/products.service';
+import { FormGroup, NgForm } from '@angular/forms';
 
 
 @Component({
@@ -13,65 +15,58 @@ export class AppComponent{
   title = 'AngularHttpRequest';
   allProducts : Product[] = [];
   isFetching: boolean;
+  myForm : NgForm;
+  productsForm: any;
   
-  constructor(private http: HttpClient){
+  constructor(private http: HttpClient, private productService: productService){
 
   }
 
   ngOnInit(){
-    this.fetchProducts();
-  }
-
-  onProductsFetch(){
-    this.fetchProducts();
-  }
-
-
-  onProductCreate(products: {pName:string, desc:string, price:string})
-  {
-    console.log(products);
-    const myHeader = new HttpHeaders({'myHeader':'proacademy'});
-    this.http.post<{ name : string }>('https://procademy-c9684-default-rtdb.firebaseio.com/products.json', products, {headers: myHeader})
-    .subscribe((data)=>{
-      console.log(data);
-    }) 
-    //will be automatically converted to a json file by the post method
-    //post method returns an observable, need a subscriber to return the data
-    //no subscriber, no request will be sent
-  }
-
-  private fetchProducts(){
     this.isFetching = true;
-    this.http.get<{[key: string] : Product}>('https://procademy-c9684-default-rtdb.firebaseio.com/products.json')
-    .pipe(map((res)=>{
-      const products = [];
-      for(const key in res)
-      { 
-        if(res.hasOwnProperty(key)){
-          products.push({...res[key] , id: key})
-        }
-      }
-      return products;
-    }))
-    .subscribe((response) => {
+    this.productService.fetchProduct().subscribe((response) => {
       console.log(response);
       this.allProducts = response;
       this.isFetching = false;
-    })
+    });
+  }
+
+  // ngOnChanges(){
+  //   this.isFetching = true;
+  //   this.productService.fetchProduct().subscribe((response) => {
+  //     console.log(response);
+  //     this.allProducts = response;
+  //     this.isFetching = false;
+  //   });
+  // }
+
+  onProductCreate(products: {pName:string, desc:string, price:string},  productsForm)
+  {
+   this.productService.createProduct(products)
+   productsForm.reset()
+  }
+
+  
+
+  onProductsFetch(){
+    this.isFetching = true;
+    this.productService.fetchProduct().subscribe((response) => {
+      console.log(response);
+      this.allProducts = response;
+      this.isFetching = false;
+    });
   }
 
   onDeleteProduct(id : string){
-    this.http.delete('https://procademy-c9684-default-rtdb.firebaseio.com/products/' + id + '.json')
-    .subscribe();
+    this.productService.deleteProduct(id).subscribe( (data) => {
+      this.allProducts.filter(product => product.id !== id)
+    });
   }
 
   onDeletAllProducts(){
-    this.http.delete('https://procademy-c9684-default-rtdb.firebaseio.com/products.json')
-    .subscribe();
+    this.productService.clearProduct().subscribe()
   }
 
 }
-
-
 
 
---
+++ AngularHttpRequest - START/AngularHttpRequest - START/src/app/service/products.service.ts
@@ -0,0 +1,54 @@
+import { HttpClient , HttpHeaders } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Product } from "../model/products";
+import { map } from "rxjs";
+
+@Injectable({providedIn: "root"}) export class productService {
+    isFetching: boolean; //similar to mentioning in provider in app.module
+    allProducts: any[];
+
+    constructor(private http : HttpClient){
+
+    }
+
+    createProduct(products: {pName:string, desc:string, price:string})
+    {
+        console.log(products);
+        const myHeader = new HttpHeaders({'myHeader':'proacademy'});
+        return this.http.post<{ name : string }>('https://procademy-c9684-default-rtdb.firebaseio.com/products.json', products, {headers: myHeader})
+        .subscribe((data)=>{
+          console.log(data);
+        }) 
+
+        
+        //will be automatically converted to a json file by the post method
+        //post method returns an observable, need a subscriber to return the data
+        //no subscriber, no request will be sent
+    }   
+
+    fetchProduct(){
+
+            this.isFetching = true;
+            return  this.http.get<{[key: string] : Product}>('https://procademy-c9684-default-rtdb.firebaseio.com/products.json')
+            .pipe(map((res)=>{
+              const products = [];
+              for(const key in res)
+              { 
+                if(res.hasOwnProperty(key)){
+                  products.push({...res[key] , id: key})
+                }
+              }
+              return products;
+            }))   
+    }
+
+    deleteProduct( id: string){
+        return this.http.delete('https://procademy-c9684-default-rtdb.firebaseio.com/products/' + id + '.json')
+       
+    }
+
+    clearProduct(){
+        return this.http.delete('https://procademy-c9684-default-rtdb.firebaseio.com/products.json')
+    }
+
+}
