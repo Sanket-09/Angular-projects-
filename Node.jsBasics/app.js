d40c6d954b85900a11a6b8bb96d58e0d0efbc2a5
Sanket Jaiswal <sanket.jaiswal@heaps.ai>
2024-02-13 00:28:11 +0530

    implemented write and read file synchronously in node.js

--- Node.jsBasics/app.js
+++ Node.jsBasics/app.js
@@ -1,16 +1,27 @@
 const readline = require('readline');
 
-const rl = readline.createInterface({
-    input : process.stdin,
-    output : process.stdout
-})
+// const rl = readline.createInterface({
+//     input : process.stdin,
+//     output : process.stdout
+// })
 
-rl.question("please enter your name: ", (name)=>{
-    console.log("You entered :" + name);
-    rl.close();
-})
+// rl.question("please enter your name: ", (name)=>{
+//     console.log("You entered :" + name);
+//     rl.close();
+// })
 
-rl.on('close', () =>{
-    console.log("Interface closed");
-    process.exit(404);
-})
+// rl.on('close', () =>{
+//     console.log("Interface closed");
+//     process.exit(404);
+// })
+
+
+const fs = require('fs');
+
+//reading and writing to a file
+let textIn = fs.readFileSync('./input.txt', 'utf-8')
+console.log(textIn);
+
+let content = `data read from input.txt: ${textIn} \n Date created: ${new Date()}`
+
+fs.writeFileSync('./output.txt', content)
