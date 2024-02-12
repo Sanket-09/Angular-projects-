9fc325e425ffe406fab4161e69188622925162be
Sanket Jaiswal <sanket.jaiswal@heaps.ai>
2024-02-13 01:22:19 +0530

    creating and starting a server

--- Node.jsBasics/app.js
+++ Node.jsBasics/app.js
@@ -1,38 +1,52 @@
-const readline = require('readline');
+// const readline = require('readline');
 
-// const rl = readline.createInterface({
-//     input : process.stdin,
-//     output : process.stdout
-// })
+// // const rl = readline.createInterface({
+// //     input : process.stdin,
+// //     output : process.stdout
+// // })
 
-// rl.question("please enter your name: ", (name)=>{
-//     console.log("You entered :" + name);
-//     rl.close();
-// })
+// // rl.question("please enter your name: ", (name)=>{
+// //     console.log("You entered :" + name);
+// //     rl.close();
+// // })
 
-// rl.on('close', () =>{
-//     console.log("Interface closed");
-//     process.exit(404);
-// })
+// // rl.on('close', () =>{
+// //     console.log("Interface closed");
+// //     process.exit(404);
+// // })
 
 
-const fs = require('fs');
+// const fs = require('fs');
 
-// //reading and writing to a file
-// let textIn = fs.readFileSync('./input.txt', 'utf-8')
-// console.log(textIn);
+// // //reading and writing to a file
+// // let textIn = fs.readFileSync('./input.txt', 'utf-8')
+// // console.log(textIn);
 
-// let content = `data read from input.txt: ${textIn} \n Date created: ${new Date()}`
+// // let content = `data read from input.txt: ${textIn} \n Date created: ${new Date()}`
 
-// fs.writeFileSync('./output.txt', content);
+// // fs.writeFileSync('./output.txt', content);
 
-// console.log()
+// // console.log()
 
-//reading and writing file asynchronously
-fs.readFile('./start.txt', 'utf-8' , (error, data) => {
-    console.log(data);
-    fs.readFile(`./${data}.txt`, 'utf-8' , (error1, data1)=>{
-        console.log(data1);
-    })
+// //reading and writing file asynchronously
+// fs.readFile('./start.txt', 'utf-8' , (error, data) => {
+//     console.log(data);
+//     fs.readFile(`./${data}.txt`, 'utf-8' , (error1, data1)=>{
+//         console.log(data1);
+//     })
+// });
+// console.log('Reading file')
+
+// const readLine = require('readLine');
+// const fs = require('fs');
+const http = require('http');
+
+const server = http.createServer((req,res)=>{
+    console.log("a new request has been received");
+    console.log(req);
 });
-console.log('Reading file')
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('the server has started')
+});
+
