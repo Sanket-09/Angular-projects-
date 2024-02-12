7f4f941981ec4a6b240bd26db2a8898f4d3ce1da
Sanket Jaiswal <sanket.jaiswal@heaps.ai>
2024-02-13 00:19:48 +0530

    intro to node.js

---
+++ Node.jsBasics/app.js
@@ -0,0 +1,16 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input : process.stdin,
+    output : process.stdout
+})
+
+rl.question("please enter your name: ", (name)=>{
+    console.log("You entered :" + name);
+    rl.close();
+})
+
+rl.on('close', () =>{
+    console.log("Interface closed");
+    process.exit(404);
+})
