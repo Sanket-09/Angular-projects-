de0a85e6d8083856e6db91bb2d5fcc60a40154ea
Sanket Jaiswal <sanket.jaiswal@heaps.ai>
2024-02-12 13:31:19 +0530

    implemented pipes | pure and impure pipes

--- angularPipes/src/app/app.component.ts
+++ angularPipes/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StudentService } from './students.service';
+import { Student } from './student';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers : [StudentService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  
   title = 'angularPipes';
+  students : Student[] | undefined;
+  totalMarks : number | undefined;
+
+  constructor(private studentService : StudentService)
+  {
+    
+  }
+
+  ngOnInit(): void {
+    this.students = this.studentService.students;
+    this.totalMarks = this.studentService.totalMarks;
+  }
 }
