9c9f7928dfd517b2b99fc5110993a53d0b0d6a5d
Sanket Jaiswal <sanket.jaiswal@heaps.ai>
2024-02-15 18:19:20 +0530

    UI skeleton completed - detailing and filter logic to be done

--- figmaProject/src/app/modules/component/filter-content/filter-content.component.ts
+++ figmaProject/src/app/modules/component/filter-content/filter-content.component.ts
@@ -7,6 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FilterContentComponent implements OnInit {
 
+  cards = [
+    { icon: 'hourglass-half', value1: 10, value2: 'Pending', bgColor: 'white' },
+    { icon: 'thumbs-up', value1: 22, value2: 'Received', bgColor: 'white' },
+    { icon: 'check-circle', value1: 123, value2: 'Closed', bgColor: 'white' },
+    { icon: 'users', value1: 619, value2: 'Total Request', bgColor: 'white' }
+  ];
+
+  selectedCard: any;
+  selectedCardIcon: any;
+
+  selectCard(card: any) {
+    this.selectedCard = card;
+  }
+
+  isSelected(card: any): boolean {
+    return this.selectedCard === card;
+  }
+
+  selectCardIcon(card: any) {
+    this.selectedCard = card;
+  }
+
+  isSelectedIcon(card: any): boolean {
+    return this.selectedCard === card;
+  }
+
   constructor() { }
 
   ngOnInit(): void {
--- figmaProject/src/app/modules/component/table/table.component.ts
+++ figmaProject/src/app/modules/component/table/table.component.ts
@@ -1,11 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface PeriodicElement {
+  prefDate: Date;
+  status: string;
+  id: string;
+  name: string;
+  reqDate : Date;
+  speciality : string;
+  visitType : string
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {status: 'Pending', prefDate: new Date(), id: 'M37281', name: 'Jane Doe' , reqDate: new Date(), speciality: 'Cardiology' , visitType: 'Escalation'},
+  {status: 'Pending', prefDate: new Date(), id: 'M37281', name: 'Jane Doe' , reqDate: new Date(), speciality: 'Cardiology' , visitType: 'Escalation'},
+  {status: 'Pending', prefDate: new Date(), id: 'M37281', name: 'Jane Doe' , reqDate: new Date(), speciality: 'Cardiology' , visitType: 'Escalation'},
+  {status: 'Pending', prefDate: new Date(), id: 'M37281', name: 'Jane Doe' , reqDate: new Date(), speciality: 'General Medicine' , visitType: 'Escalation'},
+  {status: 'Pending', prefDate: new Date(), id: 'M37281', name: 'Jane Doe' , reqDate: new Date(), speciality: 'Cardiology' , visitType: 'Escalation'},
+  {status: 'Pending', prefDate: new Date(), id: 'M37281', name: 'Jane Doe' , reqDate: new Date(), speciality: 'Cardiology' , visitType: 'Other Appointments'},
+  {status: 'Pending', prefDate: new Date(), id: 'M37281', name: 'Jane Doe' , reqDate: new Date(), speciality: 'Cardiology' , visitType: 'Escalation'},
+  {status: 'Pending', prefDate: new Date(), id: 'M37281', name: 'Jane Doe' , reqDate: new Date(), speciality: 'Cardiology' , visitType: 'Escalation'},
+
+
+];
+
+
+
+
+
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss']
 })
 export class TableComponent implements OnInit {
+
+  displayedColumns: string[] = ['status', 'prefDate', 'id', 'name', 'reqDate', 'speciality' , 'visitType'];
+  dataSource = ELEMENT_DATA;
 
   constructor() { }
 
