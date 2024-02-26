9a7c5f8d5efbdb5b392ee78121e4c36e9ac85cd1
Sanket Jaiswal <sanket.jaiswal@heaps.ai>
2024-02-26 14:31:34 +0530

    Performed crud operations with jsonFile

---
+++ crud-operations-with-jsonFile/route/account.js
@@ -0,0 +1,77 @@
+const express = require('express')
+const accountRoutes = express.Router()
+const fs = require('fs')
+
+const dataPath = './details/account.json'
+
+const saveAccountData = (data) => {
+  const stringifyData = JSON.stringify(data)
+  fs.writeFileSync(dataPath, stringifyData)
+}
+
+const getAccountData = () => {
+  const jsonData = fs.readFileSync(dataPath)
+  return JSON.parse(jsonData)
+}
+
+// reading the data
+accountRoutes.get('/account', (req, res) => {
+  fs.readFile(dataPath, 'utf8', (err, data) => {
+    if (err) {
+      throw err
+    }
+
+    res.send(JSON.parse(data))
+  })
+})
+
+accountRoutes.post('/account/addaccount', (req, res) => {
+  var existAccounts = getAccountData()
+  const newAccountId = Math.floor(100000 + Math.random() * 900000)
+
+  existAccounts[newAccountId] = req.body
+
+  console.log(existAccounts)
+
+  saveAccountData(existAccounts)
+  res.send({ success: true, msg: 'account data added successfully' })
+})
+
+accountRoutes.get('/account/list', (req, res) => {
+  const accounts = getAccountData()
+  res.send(accounts)
+})
+
+accountRoutes.put('/account/:id', (req, res) => {
+  var existAccounts = getAccountData()
+  fs.readFile(
+    dataPath,
+    'utf8',
+    (err, data) => {
+      const accountId = req.params['id']
+      existAccounts[accountId] = req.body
+
+      saveAccountData(existAccounts)
+      res.send(`accounts with id ${accountId} has been updated`)
+    },
+    true
+  )
+})
+
+accountRoutes.delete('/account/delete/:id', (req, res) => {
+  fs.readFile(
+    dataPath,
+    'utf8',
+    (err, data) => {
+      var existAccounts = getAccountData()
+
+      const userId = req.params['id']
+
+      delete existAccounts[userId]
+      saveAccountData(existAccounts)
+      res.send(`accounts with id ${userId} has been deleted`)
+    },
+    true
+  )
+})
+module.exports = accountRoutes
--- figmaProject/README.md
+++ figmaProject/README.md
@@ -1,27 +1,19 @@
-# FigmaProject
+firebase initialisation docs
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.2.1.
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-## Development server
+// Your web app's Firebase configuration
+const firebaseConfig = {
+apiKey: "AIzaSyCVrfohfFOa5-b\_\_ybih5hBj9Xix6xAt6c",
+authDomain: "figmaproject-2ad36.firebaseapp.com",
+projectId: "figmaproject-2ad36",
+storageBucket: "figmaproject-2ad36.appspot.com",
+messagingSenderId: "377377149139",
+appId: "1:377377149139:web:e9088fa6cb491255d38426"
+};
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
-
-## Code scaffolding
-
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
-
-## Build
-
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory.
-
-## Running unit tests
-
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To use this command, you need to first add a package that implements end-to-end testing capabilities.
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
