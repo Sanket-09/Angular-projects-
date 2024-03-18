b3464cb23857da6304a01832ac959fbfad3e1238
Sanket Jaiswal <sanket.jaiswal@heaps.ai>
2024-03-18 17:58:42 +0530

    updated notes for node.js till lec20 proacademy event driven architecture

--- Node.jsBasics/dist/app.dev.js
+++ Node.jsBasics/dist/app.dev.js
@@ -1,32 +1,25 @@
-// const readline = require('readline');
+"use strict";
 
+// const readline = require('readline');
 // // const rl = readline.createInterface({  //create interfact is a method on object readline
 // //     input : process.stdin,
 // //     output : process.stdout
 // // })
-
 // // rl.question("please enter your name: ", (name)=>{
 // //     console.log("You entered :" + name);
 // //     rl.close();
 // // })
-
 // // rl.on('close', () =>{     //listens to an event, here that event is close
 // //     console.log("Interface closed");
 // //     process.exit(404);
 // // })
-
 // const fs = require('fs');
-
 // // //reading and writing to a file
 // // let textIn = fs.readFileSync('./input.txt', 'utf-8')
 // // console.log(textIn);
-
 // // let content = `data read from input.txt: ${textIn} \n Date created: ${new Date()}`
-
 // // fs.writeFileSync('./output.txt', content);
-
 // // console.log()
-
 // //reading and writing file asynchronously
 // fs.readFile('./start.txt', 'utf-8' , (error, data) => {
 //     console.log(data);
@@ -35,71 +28,68 @@
 //     })
 // });
 // console.log('Reading file')
-
 // const readLine = require('readLine');
 // const fs = require('fs');
 // const http = require('http');
-
 // const server = http.createServer((req,res)=>{
 //     console.log("a new request has been received");
 //     console.log(req);
 // });
-
 // server.listen(8000, '127.0.0.1', () => {
 //     console.log('the server has started')
 // });
-
 // ************************HTTP REQUEST AND RESPONSE****************
+var fs = require('fs');
 
-const fs = require('fs')
-const http = require('http')
-const readline = require('readline') //require('readline') returns an object
-const url = require('url') //returns an urk object
-const replaceHtml = require('./modules/replaceHTML')
+var http = require('http');
 
-const html = fs.readFileSync('./Template/index.html', 'utf-8')
-let products = JSON.parse(fs.readFileSync('./Data/product.json', 'utf-8'))
-const productListHTML = fs.readFileSync('./Template/productList.html', 'utf-8')
-const productDetailHTML = fs.readFileSync(
-  './Template/product-details.html',
-  'utf-8'
-)
+var readline = require('readline'); //require('readline') returns an object
 
-const server = http.createServer((req, res) => {
-  let { query, pathname: path } = url.parse(req.url, true)
 
-  // let path = req.url;
+var url = require('url'); //returns an urk object
+
+
+var replaceHtml = require('./modules/replaceHTML');
+
+var html = fs.readFileSync('./Template/index.html', 'utf-8');
+var products = JSON.parse(fs.readFileSync('./Data/product.json', 'utf-8'));
+var productListHTML = fs.readFileSync('./Template/productList.html', 'utf-8');
+var productDetailHTML = fs.readFileSync('./Template/product-details.html', 'utf-8');
+var server = http.createServer(function (req, res) {
+  var _url$parse = url.parse(req.url, true),
+      query = _url$parse.query,
+      path = _url$parse.pathname; // let path = req.url;
+
 
   if (path === '/' || path.toLocaleLowerCase() === '/home') {
-    res.writeHead(200)
-    res.end(html.replace('{{%CONTENT%}}', 'You are in home page'))
+    res.writeHead(200);
+    res.end(html.replace('{{%CONTENT%}}', 'You are in home page'));
   } else if (path.toLocaleLowerCase() === '/about') {
     res.writeHead(200, {
       'Content-Type': 'text/html',
-      'my-header': 'Hello world',
-    })
-    res.end(html.replace('{{%CONTENT%}}', 'You are in About Page'))
+      'my-header': 'Hello world'
+    });
+    res.end(html.replace('{{%CONTENT%}}', 'You are in About Page'));
   } else if (path.toLocaleLowerCase() === '/contact') {
-    res.writeHead(200)
-    res.end(html.replace('{{%CONTENT%}}', 'You are in Contact Page'))
+    res.writeHead(200);
+    res.end(html.replace('{{%CONTENT%}}', 'You are in Contact Page'));
   } else if (path.toLocaleLowerCase() === '/products') {
     if (!query.id) {
-      let productHTMLArray = products.map((prod) => {
-        return replaceHtml(productListHTML, prod)
-      })
-      res.writeHead(200)
-      res.end(html.replace('{{%CONTENT%}}', productHTMLArray))
+      var productHTMLArray = products.map(function (prod) {
+        return replaceHtml(productListHTML, prod);
+      });
+      res.writeHead(200);
+      res.end(html.replace('{{%CONTENT%}}', productHTMLArray));
     } else {
-      let prod = products[query.id]
-      let productDetailResponseHTML = replaceHtml(productDetailHTML, prod)
-      res.end(html.replace('{{%CONTENT%}}', productDetailResponseHTML))
+      var prod = products[query.id];
+      var productDetailResponseHTML = replaceHtml(productDetailHTML, prod);
+      res.end(html.replace('{{%CONTENT%}}', productDetailResponseHTML));
     }
   } else {
-    res.writeHead(404)
-    res.end(html.replace('{{%CONTENT%}}', 'You are in Default page'))
+    res.writeHead(404);
+    res.end(html.replace('{{%CONTENT%}}', 'You are in Default page'));
   }
-})
-
-server.listen(8000, '127.0.0.1', () => {
-  console.log('The server has been started')
-})
+});
+server.listen(8000, '127.0.0.1', function () {
+  console.log('The server has been started');
+});
